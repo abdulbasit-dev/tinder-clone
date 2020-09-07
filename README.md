63bfe5658a63ef505200fc9dc5bc6b8b39e3fc0b
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-09-07 19:59:44 +0300

    finish header

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 packages usein this project:
 
 - react-router-dom
+- yarn add @material-ui/core
+- yarn add @material-ui/icons
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import './Header'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person'
+import ForumIcon from '@material-ui/icons/Forum'
+import IconButton from '@material-ui/core/IconButton'
 
 function Header() {
   return (
-    <div>
-      <h1>i am a header</h1>
+    <div className='header'>
+      <IconButton>
+        <PersonIcon fontSize='large' />
+      </IconButton>
+      <img
+        src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'
+        alt='tinder logo'
+        className='header__logo'
+      />
+      <IconButton>
+        <ForumIcon fontSize='large' />
+      </IconButton>
     </div>
   )
 }
